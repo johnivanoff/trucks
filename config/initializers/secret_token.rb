f90a512fbe8b239a574b638047d67fc6537d05d7
John Ivanoff <john.ivanoff@gmail.com>
2011-01-28 20:43:35 -0600

    trucks Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trucks::Application.config.secret_token = '1c27f372af7c24a11606e6d452ba3feb9314908c57598d146e5fe7a27bf7d8fc7c7390ae499b72f9562ec5246a304ac480ba3740e5efb57630434a7f32faaf60'
